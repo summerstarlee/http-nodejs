0d2efe4023f5910daeb961566959e4d70fa81fef
lishun_li <lishun_li@youzan.com>
2023-05-29 15:18:41 +0800

    feat: 使用 bing chat

--- server.mjs
+++ server.mjs
@@ -7,6 +7,6 @@ import router from './routes/index.mjs'
 router(app)
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`koa app listening on port ${process.env.PORT}`)
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`koa app listening on port ${process.env.PORT || 3000}`)
 });
